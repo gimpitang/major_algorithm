18e7419c95475ef414816cf72a5ca4f2f08cf745
gimpitang <dudwodpdy@gmail.com>
2024-12-23 19:08:16 +0900

    major algorithm

--- src/A1sort/A102명예의전당.java
+++ src/A1sort/A102명예의전당.java
@@ -5,25 +5,22 @@ import java.util.*;
 
 public class A102명예의전당 {//      프로그래머스
     public static void main(String[] args) {
-        int k = 3;
-        int [] score ={10,100,20,1150,1,100,200};
+        int k = 4;
+        int [] score ={0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
         Queue<Integer> pq = new PriorityQueue<>();
         int [] answer = new int[score.length];
 
-        for(int i =0; i< answer.length;i++){
+        for(int i=0; i< score.length;i++){
+
             pq.add(score[i]);
+            if(pq.size()>k){
+                pq.poll();
+            }
             answer[i]=pq.peek();
         }
-
-
-
-
-
+        System.out.println(pq);
+        System.out.println(Arrays.toString(answer));
 
     }
-
-
-
-
 
 }
