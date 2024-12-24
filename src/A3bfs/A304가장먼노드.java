6ad83b55acb48aaffb3bfc6aa476bcacbe0a422b
gimpitang <dudwodpdy@gmail.com>
2024-12-24 18:20:36 +0900

    major algorithm

--- src/A3bfs/A304가장먼노드.java
+++ src/A3bfs/A304가장먼노드.java
@@ -1,7 +1,60 @@
 package A3bfs;
 
+import java.util.*;
+
 public class A304가장먼노드 {
     public static void main(String[] args) {
+
+        int[][] edge = {{3,6},{4,3},{3,2},{1,3},{1,2},{2,4},{5,2}};
+        //     0에서 출발하여 4번node까지 도달하는 최단거리 구하기
+        int targetNode =4;
+        int n = 6;
+
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        for (int i =0; i<n+1;i++){
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] a :edge){
+            adjList.get((a[0])).add(a[1]);
+            adjList.get((a[1])).add(a[0]);
+        }
+
+        for(List<Integer>a: adjList){
+            a.sort(Comparator.naturalOrder());
+        }
+        System.out.println(adjList);
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> myQueue =new LinkedList<>();
+        myQueue.add(1);
+        visited[1]=true;
+        int[] distance =new int[n+1];
+        while (!myQueue.isEmpty()){
+            int temp = myQueue.poll();
+            System.out.println(temp);
+            for(int target : adjList.get(temp)){
+
+                if(!visited[target]){
+
+                    myQueue.add(target);
+                    visited[target]=true;
+                    distance[target]=distance[temp]+1;
+
+
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(distance));
+        Arrays.sort(distance);
+        int count=0;
+        for(int i=0; i<distance.length;i++){
+            if(distance[i]==distance[distance.length-1]){
+                count++;
+            }
+        }
+        System.out.println(count);
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,21 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int [] intArr = new int[input.length];
-        for(int i =0; i<input.length;i++){
-            intArr[i]=Integer.parseInt(input[i]);
-        }
-//        System.out.println(Arrays.toString(intArr));
 
-        List<List<Integer>> myList = new ArrayList<>();
-        for (int i =0; i<intArr[0]+1; i++){
-            myList.add(new ArrayList<>());
-        }
-        for(int i=0; i<intArr[1];i++){
-            String[] line = br.readLine().split(" ");
-            myList.get(Integer.parseInt(line[0])).add(Integer.parseInt(line[1]));
-            myList.get(Integer.parseInt(line[1])).add(Integer.parseInt(line[0]));
-        }
-        for(List<Integer>a: myList){
-            a.sort(Comparator.naturalOrder());
-        }
-        boolean [] visited1 = new boolean[intArr[0]+1];
-        boolean [] visited2 = new boolean[intArr[0]+1];
+        int[] answer = {};
+        int numer1 =1;
+        int denom1=2;
+        int numer2 =3;
+        int denom2=4;
 
-        DFS(myList,intArr[2],visited1);
-        Queue<Integer> myQueue =new LinkedList<>();
-        BFS(myList,intArr[2],visited2,myQueue);
+        int num3= numer1 *denom2+ numer2 *denom1;
+        int denom3 = denom1*denom2;
 
 
 
-    }
-    static void DFS(List<List<Integer>> myList, int start,boolean[] visited){
-        StringBuilder sb = new StringBuilder();
-        sb.append(start).append(" ");
-//        System.out.println(start);
-        visited[start]=true;
-
-        for (int a : myList.get(start)) {
-            if(visited[a]==false) {
-                DFS(myList, a, visited);
-                sb.append(myList.get(start)).append(" ");
-            }
-        }
-        System.out.println(sb.toString().trim());
 
 
-    }
-    static void BFS(List<List<Integer>> myList, int start,boolean[] visited,Queue<Integer> myQueue){
-            StringBuilder sb = new StringBuilder();
-//        System.out.println(start);
-        myQueue.offer(start);
-        sb.append(start).append(" ");
-        visited[start]=true;
-
-        while (!myQueue.isEmpty()){
-            int temp = myQueue.poll();
-
-//            System.out.println(temp);
-            for(int target : myList.get(temp)){
-                if(!visited[target]){
-                    myQueue.add(target);
-                    sb.append(target).append(" ");
-                    //      Queue에 add하는 시점에 boolean 값 세팅
-                    visited[target]=true;
-                }
-
-            }
-        }
-            System.out.println(sb.toString().trim());
 
 
     }
